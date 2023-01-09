d2cff60623f2d5c3349ecb9785ae9743f5672463
ManolyaGursoy <gursoymanolya@gmail.com>
2023-01-09 18:44:49 +0300

    console-log-duzenlendi

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ import axios from "axios";
  const getData = async (user_id) => {
     const { data: user } = await axios(`https://jsonplaceholder.typicode.com/users/${user_id}`);
     const { data: posts } = await axios(`https://jsonplaceholder.typicode.com/posts?userId=${user_id}`);
-    console.log({user,posts});
+    const resultData = {"User": user,"Posts": posts}
+    console.log(resultData);
 }
 
 export default getData;
